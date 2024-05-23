b6b0c9ae0450c23714eef391845626ab3bc81772
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-05-23 03:53:51 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Two blank lines before the function definition
 @app.route('/', strict_slashes=False)
 def index():
     """Display 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+# No blank line at the end of the file
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
