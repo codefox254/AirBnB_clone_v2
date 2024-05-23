0166c1d123a04eeb7ee19d8cf6c225e1b012f3e6
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-05-23 03:55:00 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Two blank lines before the function definition
+
 @app.route('/', strict_slashes=False)
 def index():
     """Display 'Hello HBNB!'"""
